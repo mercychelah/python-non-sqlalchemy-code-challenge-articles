2e3897f3660569052e451217551af6a7a819d367
mercy <mercychelah27@gmail.com>
2024-06-13 00:18:46 -0400

    complete code

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,17 +1,18 @@
-#class article initialize
 class Article:
-    all= []  
-    def _init_(self, author, magazine, title):
+    all = []  
+
+    def __init__(self, author, magazine, title):
         if not isinstance(title, str) or not (5 <= len(title) <= 50):
             raise ValueError("Title must be a string between 5 and 50 characters")
-        
+
         self._author = author
         self._magazine = magazine
         self._title = title
+
         author._articles.append(self)
         magazine._articles.append(self)
         Article.all.append(self)
-    
+
     @property
     def title(self):
         return self._title
@@ -39,9 +40,10 @@ class Article:
         self._author._articles.remove(self)
         value._articles.append(self)
         self._author = value
- #author class initialized
+
+
 class Author:
-    def _init_(self, name):
+    def __init__(self, name):
         if not isinstance(name, str) or len(name) == 0:
             raise ValueError("Name must be a non-empty string")
         self._name = name
@@ -51,15 +53,18 @@ class Author:
     def name(self):
         return self._name
 
+    @property
     def articles(self):
         return self._articles
 
+    @property
     def magazines(self):
         return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
         return Article(self, magazine, title)
 
+    @property
     def topic_areas(self):
         if not self._articles:
             return None
@@ -67,11 +72,12 @@ class Author:
 
     def remove_article(self, article):
         self._articles.remove(article)
-# magazine class initialized
+
+
 class Magazine:
     _magazines = []
 
-    def _init_(self, name, category):
+    def __init__(self, name, category):
         if not isinstance(name, str) or not (2 <= len(name) <= 16):
             raise ValueError("Name must be a string with 2 to 16 characters.")
         if not isinstance(category, str) or len(category) == 0:
@@ -84,7 +90,7 @@ class Magazine:
     @property
     def name(self):
         return self._name
-#setters
+
     @name.setter
     def name(self, value):
         if not isinstance(value, str) or not (2 <= len(value) <= 16):
@@ -101,27 +107,29 @@ class Magazine:
             raise ValueError("Category must be a non-empty string.")
         self._category = value
 
+    @property
     def articles(self):
         return self._articles
 
+    @property
     def contributors(self):
         return list({article.author for article in self._articles})
 
+    @property
     def article_titles(self):
         if not self._articles:
             return None
         return [article.title for article in self._articles]
 
+    @property
     def contributing_authors(self):
         from collections import Counter
         author_count = Counter(article.author for article in self._articles)
         contributing_authors = [author for author, count in author_count.items() if count > 2]
         return contributing_authors if contributing_authors else None
 
-    #class method for top publisher
     @classmethod
     def top_publisher(cls):
         if not cls._magazines:
-           return None
-        else:
-           return max(cls._magazines, key=lambda magazine: len(magazine.articles()))
+            return None
+        return max(cls._magazines, key=lambda magazine: len(magazine.articles))
